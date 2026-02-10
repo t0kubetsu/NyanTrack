2eff1c09b7b0523f4bef5d9e70f838bb0f17ab08
t0kubetsu <alexis.danjon@nc3.lu>
2026-02-10 11:23:39 +0100

    remove native implementation

--- android/app/src/main/kotlin/com/example/app/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.app
-
-object AppConstants {
-    const val SERVER_BASE_URL = "https://yamileth-hypobaric-victoria.ngrok-free.dev"
-    const val DEVICE_ENDPOINT = "$SERVER_BASE_URL/device"
-    const val LOCATION_ENDPOINT = "$SERVER_BASE_URL/location"
-}
